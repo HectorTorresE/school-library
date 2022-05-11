bb6712cd998c9b3d6de2c6afdd589299c02e0fdb
HectorTorresE <hectorjte@gmail.com>
2022-05-11 14:52:16 -0600

    Add associations

---
+++ book.rb
@@ -0,0 +1,14 @@
+class Book
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def add_rental(rental)
+    @rentals.push(rental)
+  end
+end
